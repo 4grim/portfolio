a8b877c934edbac7a47f095e3b2863a50e2d3de7
4grim <amber.grimaldi@gmail.com>
2014-08-08 14:10:27 -0600

    updated resume models

--- portfolio/resume/models.py
+++ portfolio/resume/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+
+class Technology(models.Model):
+	name = models.CharField(max_length=200)
+
+	def __unicode__(self):
+		return self.name
+
+
+class Industry(models.Model):
+	name = models.CharField(max_length=200)
+
+	def __unicode__(self):
+		return self.name
+
+
+class Client(models.Model):
+	name = models.CharField(max_length=200)
+	website = models.URLField()
+	email = models.EmailField(max_length=254)
+	contact_person = models.CharField(max_length=200)
+	industries = models.ManyToManyField(Industry)
+
+	def __unicode__(self):
+		return self.name
+
+
+class ProjectImage(models.Model):
+	title = models.CharField(max_length=200)
+	description = models.TextField(blank=True)
+	image = models.ImageField(upload_to='resume')
+
+	def __unicode__(self):
+		return self.title
+
+
+class Project(models.Model):
+	title = models.CharField(max_length=200)
+	description = models.TextField(blank=True)
+	client = models.ForeignKey(Client)
+	images = models.ManyToManyField(ProjectImage, blank=True)
+	start_date = models.DateField()
+	end_date = models.DateField()
+	position = models.CharField(max_length=200)
+	technologies = models.ManyToManyField(Technology)
+
+	def __unicode__(self):
+		return self.title
+
+
+
+
+
+
