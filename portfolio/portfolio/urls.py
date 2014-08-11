398d5f4cc6ebdedbb22e764b0a6452cbc0008f68
4grim <amber.grimaldi@gmail.com>
2014-08-10 22:23:12 -0600

    got base urls set up

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -1,12 +1,16 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'portfolio.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+
+urlpatterns = patterns('',
+	url(r'^$', include('resume.urls')),
+	url(r'^admin/', include(admin.site.urls)),
 )
+
+
+
