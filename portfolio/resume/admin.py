fb7513f3c2fdb7f29f44df590f88e9b4faa891ae
4grim <amber.grimaldi@gmail.com>
2014-08-11 20:53:04 -0600

    updated resume app to have data edited by the admin site

--- portfolio/resume/admin.py
+++ portfolio/resume/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from resume.models import Technology, Industry, Client, Project, ProjectImage
 
-# Register your models here.
+
+admin.site.register(Technology)
+admin.site.register(Industry)
+admin.site.register(Client)
+admin.site.register(Project)
+admin.site.register(ProjectImage)
