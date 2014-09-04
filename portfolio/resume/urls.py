ca7c5b6a4cb91e42f43f9fa20e59206e1f02364a
4grim <amber.grimaldi@gmail.com>
2014-09-04 13:48:09 -0600

    implemented new landing page styles and removed the previous link to project index

--- portfolio/resume/urls.py
+++ portfolio/resume/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, url
 
-from resume.views import index, cv, project_index
+from resume.views import index, cv
 
 
 urlpatterns = patterns('',
     url(r'^$', index, name='index'),
     url(r'^resume/$', cv, name='resume'),
-    url(r'^projects/$', project_index, name="project_index")
 )
--- portfolio/resume/views.py
+++ portfolio/resume/views.py
@@ -4,19 +4,14 @@ from resume.models import Technology, Industry, Client, Project, ProjectImage
 
 def index(request):
 	context = {}
-	return render(request, 'index.html', context)
-
+	return render(request, 'project_index.html', context)
 
 def cv(request):
 	projects = Project.objects.order_by('-start_date')
 	technologies = Technology.objects.all()
+	
 	context = {
 		'projects': projects,
 		'technologies': technologies,
 	}
 	return render(request, 'resume/cv.html', context)
-
-
-def project_index(request):
-	context = {}
-	return render(request, 'resume/project_index.html', context)
