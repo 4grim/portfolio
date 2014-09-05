66b82a094dd52ba86d05bb746409820f671fa67d
4grim <amber.grimaldi@gmail.com>
2014-09-05 15:05:52 -0600

    updated script file to include automatically resizing height for width on project images

--- portfolio/static/js/script.js
+++ portfolio/static/js/script.js
@@ -1,1 +1,7 @@
-// tag cloud in cv
+// Project Image Height Generator
+$( document ).ready(function() {
+    var img = $('.project');
+    var width = $(img).css('width');
+    $(img).css('height', width);
+})
+// End Project Image Height Generator
