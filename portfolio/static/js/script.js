81a9654a04ef0096020e0450ed3f1defdc2b85bf
4grim <amber.grimaldi@gmail.com>
2014-09-05 15:57:13 -0600

    remove image resize script as no longer needed

--- portfolio/static/js/script.js
+++ portfolio/static/js/script.js
@@ -1,7 +1,1 @@
-// Project Image Height Generator
-$( document ).ready(function() {
-    var img = $('.project');
-    var width = $(img).css('width');
-    $(img).css('height', width);
-})
-// End Project Image Height Generator
+
