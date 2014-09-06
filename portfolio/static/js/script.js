424eb957be97cf154b74eec35e7b3f2ef1efa502
4grim <amber.grimaldi@gmail.com>
2014-09-05 21:21:24 -0600

    added script to highlight the current page on the nav and associated styles, also made the resume link open in new window

--- portfolio/static/js/script.js
+++ portfolio/static/js/script.js
@@ -1,1 +1,10 @@
-
+// Main Site Navigation Highlighting for Page
+$( document ).ready(function() {
+	var pathnames = location.pathname;
+	$('a.nav-link').each(function(i, o){ 
+		if ($(o).attr('href') == pathnames) {
+			$(o).css('background-color', '#59594C');
+		}
+	});
+});
+// End Main Site Navigation Highlighting for Page
